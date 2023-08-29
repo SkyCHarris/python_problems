7a9e5eade2f5a2a880d95345c9c9e15d30df4fff
SkyCHarris <skyler.iqeq@gmail.com>
2023-08-29 13:07:20 -0600

    first 8 problems

---
+++ minutes_to_seconds.py
@@ -0,0 +1,8 @@
+# Write a function that takes an integer 'minutes' and converts it to seconds
+
+def convert(minutes) :
+    seconds = minutes * 60
+    print(seconds)
+    return seconds
+  
+convert(5)
---
+++ power_calculator.py
@@ -0,0 +1,4 @@
+# Create a function that takes 'voltage' and 'current' and returns the calculated power
+
+def circuit_power(voltage, current) :
+    return voltage * current
