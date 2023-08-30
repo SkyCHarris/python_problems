292fdf24c7cdebf2971317af6f68b84d6650387f
SkyCHarris <skyler.iqeq@gmail.com>
2023-08-30 08:14:17 -0600

    convert hours to seconds problem

---
+++ hours_to_seconds.py
@@ -0,0 +1,10 @@
+# Write a function that converts hours into seconds
+
+def how_many_seconds(hours):
+    seconds_total = hours * 60 * 60
+    print(seconds_total)
+    return seconds_total
+
+how_many_seconds(2)
+how_many_seconds(10)
+how_many_seconds(24)
