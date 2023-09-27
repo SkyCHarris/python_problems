09f05b7a39db145789f741420b80ee9265e1dd38
Sky Harris <skyler.iqeq@gmail.com>
2023-09-27 12:10:43 -0600

    convert hours and minutes to seconds, and, check object equality problems

---
+++ check_object_equality.py
@@ -0,0 +1,10 @@
+# Create a function that checks to see if two object argumnets are equal to one another.
+# Return True if the objects are equal, otherwise, return False
+
+def is_equal(obj_one, obj_two):
+    if obj_one == obj_two:
+        return True
+    if obj_one != obj_two:
+        return False
+    
+
