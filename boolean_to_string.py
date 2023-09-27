6fe881364b6098e9cc3e8beea3f951c3b3f052d6
Sky Harris <skyler.iqeq@gmail.com>
2023-09-27 11:45:54 -0600

    convert boolean to string problem

---
+++ boolean_to_string.py
@@ -0,0 +1,10 @@
+# Create a function that takes a boolean variable 'flag' and returns it as a string
+
+def bool_to_string(flag):
+    string = str(flag)
+    print(string)
+    return string
+   
+
+bool_to_string(True)
+bool_to_string(False)
