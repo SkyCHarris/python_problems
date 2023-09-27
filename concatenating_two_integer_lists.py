829d897fe9af16b2eef2b4bb7ba6ff05f9dd10eb
Sky Harris <skyler.iqeq@gmail.com>
2023-09-27 14:41:16 -0600

    concatenating two integer lists problem

---
+++ concatenating_two_integer_lists.py
@@ -0,0 +1,8 @@
+# Create a function to concatenate two integer lists
+
+def concat(list1, list2):
+   merged_list = list1 + list2
+   print(merged_list)
+   return merged_list
+
+concat([1, 3, 5], [2, 6, 8])
