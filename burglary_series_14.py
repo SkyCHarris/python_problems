bb14bc3c8093a945f727500c5ffc6f804df6da1c
Sky Harris <skyler.iqeq@gmail.com>
2023-10-01 17:50:50 -0600

    burglary series and name greeting problems

---
+++ burglary_series_14.py
@@ -0,0 +1,8 @@
+# You call your spouse in anger and a 'little' argumnet takes place. Count the total amount of insults used.
+
+def total_amount_adjectives(insult):
+    insult_number = total_amount_adjectives.count(insult)
+    print(insult_number)
+    return insult_number
+
+total_amount_adjectives({ "a": "moron"})
---
+++ name_greeting.py
@@ -0,0 +1,4 @@
+# Create a funciton that takes a name and returns a greeting in the form of a string
+
+def name_greeting(name):
+    return "Hello" + name + "!"
