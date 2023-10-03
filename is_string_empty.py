00e7650bbd859ebcfbf8ef7e415e63066a3d8d13
Sky Harris <skyler.iqeq@gmail.com>
2023-10-02 23:58:30 -0600

    is string empty probem

---
+++ is_string_empty.py
@@ -0,0 +1,8 @@
+# Create a function that returns True if a string is empty and False otherwise
+
+def is_empty(string):
+    if string == "":
+        return True
+    else:
+        return False
+    
