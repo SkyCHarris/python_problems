e43c901236350c348bc2335a160e3dea620b2f15
Sky Harris <skyler.iqeq@gmail.com>
2023-10-02 20:38:12 -0600

    commpare strings and count characters problem

---
+++ compare_strings_count_characters.py
@@ -0,0 +1,4 @@
+# Create a function that takes two strings as argumnets and return either True or False depending on whether the total number of characters in the first string is equal to the total number of characters in the second string
+
+def comp(str1, str2):
+    return len(str1) == len(str2)
