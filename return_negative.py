1ca13fab48d5e4a06ecfde843b86213a8f605256
Sky Harris <skyler.iqeq@gmail.com>
2023-10-04 18:22:26 -0600

    return negative problem

---
+++ return_negative.py
@@ -0,0 +1,10 @@
+# Create a function that takes a number as an argument and returns negative of that number
+# Return negative numbers without any change
+
+# Function that returns negative number from positive
+def return_negative(num):
+   return -abs(num)
+    
+return_negative(4)
+return_negative(15)
+return_negative(-4)
