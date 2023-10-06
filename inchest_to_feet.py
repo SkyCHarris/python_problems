07c8af3036bbd64d329add315ace56d06d0d0a56
Sky Harris <skyler.iqeq@gmail.com>
2023-10-05 22:20:45 -0600

    inches to feet problem

---
+++ inchest_to_feet.py
@@ -0,0 +1,8 @@
+# Create a function that accepts a measurement value in inches and returns the equivalent of the measurement value in feet
+
+def inches_to_feet(inches):
+    return inches // 12
+
+inches_to_feet(324)
+inches_to_feet(12)
+inches_to_feet(36)
