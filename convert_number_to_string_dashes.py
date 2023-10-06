e77088472c711ef3156f419e10f51280b8d47a2c
Sky Harris <skyler.iqeq@gmail.com>
2023-10-06 13:46:50 -0600

    convert number to string of dashes problem

---
+++ convert_number_to_string_dashes.py
@@ -0,0 +1,9 @@
+# Create a function that takes a number (from 1-60) and returns a corresponding 'string' of hyphens
+
+def num_to_dashes(num):
+    num = '-' * num
+    return num
+
+num_to_dashes(1)
+num_to_dashes(5)
+num_to_dashes(3)
