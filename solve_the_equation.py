4d955f9aab71f930910aedc9d742f1b40dfb0507
Sky Harris <skyler.iqeq@gmail.com>
2023-10-11 14:55:45 -0600

    solve the equationand check string for spaces problems

---
+++ solve_the_equation.py
@@ -0,0 +1,11 @@
+
+#! Solve the Equation
+
+# Create a function that takes an equation (e.g. "1+1") and returns the answer
+
+def equation(s):
+    return eval(s)
+
+equation("1+1")
+equation("7*4-2")
+equation("1+1+1+1+1")
