6c5583a1d2846a2ec1ad4e6a1ec94f1299dba4c1
Sky Harris <skyler.iqeq@gmail.com>
2023-10-11 14:02:36 -0600

    find the smallest and biggest numbers in a list problem

---
+++ find_smallest_and_biggest_numbers.py
@@ -0,0 +1,8 @@
+
+#! Find the Smallest and Biggest Numbers
+
+# Create a function that accepts a list of numbers and return both the min and max numbers, in that order (as a list)
+
+def min_max(lst):
+    return [min(lst), max(lst)]
+
