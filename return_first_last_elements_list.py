22d21b4f1f8267b3246d54222dc4603fcf79520e
Sky Harris <skyler.iqeq@gmail.com>
2023-10-16 21:51:25 -0600

    return first and last elements of a list problem

---
+++ return_first_last_elements_list.py
@@ -0,0 +1,9 @@
+
+#! Return the First and Last Elements in a List
+
+# Create a function that takes a list of values and returns the first and last values in a new list
+
+def first_last(lst):
+    return [lst[0], lst[-1]]
+
+first_last([5, 10, 15, 20, 25])
