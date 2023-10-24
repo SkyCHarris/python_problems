1df974b55d33ae7109a5e3d8bb52ad4cbacea27a
SkyCHarris <skyler.iqeq@gmail.com>
2023-10-23 19:38:59 -1000

    (Updated for case of division) Basic Calculator problem

--- easy/basic_calculator.py
+++ easy/basic_calculator.py
@@ -6,7 +6,7 @@
 
 def calculator(num1, operator, num2):
     stringy = str(num1) + operator + str(num2)
-    if num2 == 0:
+    if num2 == 0 and operator == "/":
         return "Can't divide by 0!"
     else:
         return eval(stringy)
