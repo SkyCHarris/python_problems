90a8dcf0eead77dfb9ef523ad87276010c24a473
Sky Harris <skyler.iqeq@gmail.com>
2023-11-15 15:19:19 -0700

    Instant Jazz problem

--- easy/instant_jazz.py
+++ easy/instant_jazz.py
@@ -4,3 +4,22 @@
 # Create a function which concatenates the number 7 to the end of every chord in a list
 # Ignore all chords which already end with 7
 
+def jazzify(list):
+
+    newList = []
+
+    for i in list:
+        if i.endswith("7") == True:
+            newList.append(i)
+        elif i.endswith("7") == False:
+            i = i + "7"
+            newList.append(i)
+        elif list == []:
+            return []
+            
+    return newList
+        
+jazzify(["G", "F", "C"])
+jazzify(["Dm", "G", "E", "A"])
+jazzify(["F7", "E7", "A7", "Ab7", "Gm7", "C7"])
+jazzify([])
