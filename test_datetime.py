cd2e5cca338a7af5ee0611950d89b1cd624d2654
Sky Harris <skyler.iqeq@gmail.com>
2023-11-15 21:49:25 -0700

    Test files for research/practice

---
+++ test_datetime.py
@@ -0,0 +1,28 @@
+
+# 1. Current Date and Time
+import datetime
+
+now = datetime.datetime.now()
+
+print(now)
+
+# 2. Current Date
+
+import datetime
+
+current_date = datetime.date.today()
+
+print(current_date)
+
+# Attributes of datetime Module
+
+import datetime
+print(dir(datetime))
+
+# Test
+
+import datetime
+
+timezone = datetime.timezone.cst()
+
+print(timezone)
---
+++ test_fastest_way_loop.py
@@ -0,0 +1,9 @@
+
+def for_loop(n):
+    s = 0
+    for i in range(n):
+        s += i
+    return s
+
+for_loop(5)
+for_loop(100)
