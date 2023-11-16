785b9a36decee8cae9be3e9e2e6efc2cd8fdf7f3
Sky Harris <skyler.iqeq@gmail.com>
2023-11-15 22:13:22 -0700

    Find the Missing Number

---
+++ easy/find_missing_number.py
@@ -0,0 +1,8 @@
+
+#! Find the Missing Number
+
+# Create a function that takes a list of numbers between 1 and 10 (excludes one number) and returns the missing number
+
+
+def missing_num(lst):
+    return 55 - sum(lst)
