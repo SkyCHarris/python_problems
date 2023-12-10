2e57fc32981ae197a8dfd8c8f8fc9223e2142ddf
Sky Harris <skyler.iqeq@gmail.com>
2023-12-10 14:20:51 -0700

    Unpacking Lists problem

---
+++ easy/unpacking_lists.py
@@ -0,0 +1,13 @@
+
+# Unpacking Lists
+
+# Assign variables from lists in a succinct way with Destructuring Assignment
+
+
+writeyourcodehere = [1, 2, 3, 4, 5, 6]
+
+first, *middle, last = [1, 2, 3, 4, 5, 6]
+
+print(first)
+print(middle)
+print(last)
