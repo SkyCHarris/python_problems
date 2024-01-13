235777330a049b4eb23137a4956285524b6fbd5b
Sky Harris <skyler.iqeq@gmail.com>
2024-01-13 13:24:40 -0700

    Scraggly files

---
+++ test_lambda.py
@@ -0,0 +1,10 @@
+
+#! Lambda Functions
+
+# Little, anonymous functions. More restrictive but more concise than reg functions
+
+# -How Python lambdas came to be
+# -How lambdas compare with regular function objects
+# -How to write lambda functions
+# -Which functions in the Python standard library leverage lambdas
+# -When to use or avoid Python lambda functions
