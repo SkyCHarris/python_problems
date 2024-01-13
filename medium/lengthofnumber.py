65b22d40003ca395aac1802b6975d7d46cb2fd3c
Sky Harris <skyler.iqeq@gmail.com>
2024-01-13 13:26:25 -0700

    Failed attempt at Length of Number problem

---
+++ medium/lengthofnumber.py
@@ -0,0 +1,15 @@
+
+#! Length of Number
+
+# Create a function that takes a number num and returns its length
+
+def number_length(num):
+    num_str = str(num)
+    for i in num_str:
+        return sum(i)
+
+number_length(10)
+number_length(5000)
+number_length(0)
+
+# Attempt 2
