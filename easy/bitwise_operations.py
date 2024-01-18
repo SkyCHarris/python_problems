3c98e964333fc6a67887e226032341ef3a414e83
Sky Harris <skyler.iqeq@gmail.com>
2024-01-17 21:33:03 -0700

    Bitwise Operations problem

---
+++ easy/bitwise_operations.py
@@ -0,0 +1,20 @@
+
+#! Bitwise Operations
+
+# Write 3 functions to calculate the bitwise AND, bitwise OR, and bitwise XOR of two numbers
+
+# bitwise AND
+def bitwise_and(n1, n2):
+    return n1 & n2
+
+# bitwise OR
+def bitwise_or(n1, n2):
+    return n1 | n2
+
+# bitwise XOR
+def bitwise_xor(n1, n2):
+    return n1 ^ n2
+
+bitwise_and(7, 12)
+bitwise_or(7, 12)
+bitwise_xor(7, 12)
