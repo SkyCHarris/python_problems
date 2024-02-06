5594d6a93224ada77cd5afc5303583007d1dffb0
Sky Harris <skyler.iqeq@gmail.com>
2024-02-06 15:13:29 -0700

    Find Highest Integer in List Using Recursion problem

---
+++ medium/find_highest_int_recursion.py
@@ -0,0 +1,10 @@
+
+#! Find the Highest Integer in the List Using Recursion
+
+# Create function that finds highest integer in the list using recursion
+
+def find_highest(lst):
+    if len(lst[0]) == 1:
+        return lst[0]
+    else:
+        return max(lst[0], find_highest(lst[1:]))
